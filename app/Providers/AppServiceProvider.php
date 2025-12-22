2934ce2d1b56319090c9bbb589d6323ae5a7551b
amaheeeen <farhankholid02@gmail.com>
2025-12-22 19:23:43 +0700

    Fix mixed content error by forcing HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,30 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; 
+use Illuminate\Support\Facades\URL; // <--- INI WAJIB ADA
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Paksa HTTPS kalau di Production (Railway)
-        if($this->app->environment('production')) {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
-
-        if (config('app.env') === 'production') {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
-    }  
-}    
+    }
+}
