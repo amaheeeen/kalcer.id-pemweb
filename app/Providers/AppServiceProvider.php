f749da4994d9e4a91fd808f5618d4b18484d6f0a
amaheeeen <farhankholid02@gmail.com>
2026-01-17 14:06:32 +0700

    Fix Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
     // Paksa HTTPS jika di production
-    if($this->app->environment('production')) {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
     }
 }
